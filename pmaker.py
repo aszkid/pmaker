39c06a3a02481a89806f8c70ca046e6968cacd83
aszkid <polbox360@hotmail.com>
2013-09-11 02:12:30 +0200

    Practically working

--- pmaker.py
+++ pmaker.py
@@ -9,7 +9,8 @@ import argparse
 import subprocess
 import os
 import shutil
-
+import pprint
+import fileinput
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -18,10 +19,16 @@ parser.add_argument(
 	"language", help="The project language (c, cpp)")
 parser.add_argument(
 	"--git", help="Disable Git repository creation", action="store_true")
-parser.add_argument(
-	"--defs", help="Disable default files creation", action="store_true")
+#parser.add_argument(
+#	"--defs", help="Disable default files creation", action="store_true")
 parser.add_argument(
 	"--raze", help="Remove all contents of the project directory first", action="store_true")
+parser.add_argument(
+	"--cpp11", help="Enable C++11 (C++0X) in CMakeLists.txt", action="store_true")
+parser.add_argument(
+	"--c99", help="Enable C99 in CMakeLists.txt", action="store_true")
+parser.add_argument(
+	"--c11", help="Enable C11 in CMakeLists.txt", action="store_true")
 
 args = parser.parse_args()
 
@@ -32,7 +39,7 @@ if os.getcwd().split(os.sep)[-1] == "pmaker":
 	p_prefix = "../"
 	inside = True
 	print "We are inside the pmaker directory!"
-PSOURCE = PNAME + "source/"
+PSOURCE = PNAME + "source"
 
 def path(p):
 	return p_prefix + p
@@ -59,25 +66,47 @@ def raze(do):
 		print "Razing..."
 		execute("rm -r %s" % path(PNAME))
 
+def prepareres(res):
+	pre = "files/"
+	if not inside:
+		pre = "pmaker/" + pre
+	return pre + res
+def prepareobj(res, sub = ''):
+	return path(PSOURCE + sub + '/' + res)
+
 def copyfiles(defs):
 	general = [".gitignore", "CMakeLists.txt"]
 	specific = {
-		"cpp" : ["main.cpp"],
-		"c" : ["main.c"]
+		"cpp" : {
+			"src": ["main.cpp"]
+		},
+		"c" : {
+			"src" : ["main.c"]
+		}
 	}
-	extras = {
-		"cpp" : ["header.hpp", "header.cpp"],
-		"c" : ["header.h", "header.c"]
-	}
+	final = []
 	for f in general:
-		pre = "files/"
-		if not inside:
-			pre = "pmaker/files/"
-		print "Copying '{0}'...".format(pre + f)
-		shutil.copy(pre + f, path(PSOURCE + f))
+		final.append(dict([('from', prepareres(f)), ('to', prepareobj(f))]))
+	for lang in specific:
+		if args.language == lang:
+			for t in specific[lang]:
+				for f in specific[lang][t]:
+					final.append(dict([('from',prepareres(f)),('to',prepareobj(f,"/"+t))]))
 	
-		
+	for r in final:
+		print "Copying '{0}' to '{1}'".format(r["from"],r["to"])
+		shutil.copy(r["from"],r["to"])
+
+def cmakeset():
+	for line in fileinput.input(prepareobj("CMakeLists.txt"),inplace=1):
+		print line.replace("__projxxxname__", args.name)
+		if args.language == "cpp":
+			if args.cpp11:
+				print line.replace("")
+			
+
 if(__name__ == "__main__"):
 	raze(args.raze)
 	createdirs()
-	copyfiles(args.defs)
+	copyfiles(True) #args.defs
+	cmakeset()
